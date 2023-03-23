d0ecf6c302acd61d16728b5a895c373dc4ee83c9
biruk-tafese <tafesebiruk23@gmail.com>
2023-03-23 23:45:40 +0300

    data structure in cpp

---
+++ data.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+
+ int main() {
+
+    int myArray[5];
+     std::cout << "Enter 5 elements" << std::endl;
+     for (int s = 0; s < 4; s++) {
+        std::cin >>myArray[s];
+     }
+ }
